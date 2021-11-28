ec79023d5a2d554d395ceae4a22306eca2520a5e
Elstargo00 <a.thaprasop.j@icloud.com>
2021-11-28 15:22:23 +0700

    first complete version

---
+++ food.py
@@ -0,0 +1,15 @@
+from turtle import Turtle
+import random
+
+class Food(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.shape("circle")
+        self.shapesize(stretch_len=0.5, stretch_wid=0.5)
+        self.penup()
+        self.color("red")
+        self.speed("fastest")
+        self.refresh()
+
+    def refresh(self):
+        self.goto(random.randint(-285, 285), random.randint(-285, 285))
--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 from turtle import Screen
 from snake import Snake
+from food import Food
+from scoreboard import Scoreboard
+
 import time
 
 # screen setting:
@@ -9,20 +12,39 @@ screen.bgcolor("black")
 screen.setup(width=600, height=600)
 screen.tracer(0)
 
-a_snake = Snake()
+snake = Snake()
+food = Food()
+scoreboard = Scoreboard()
 
 screen.listen()
-screen.onkey(key="Up", fun=a_snake.up)
-screen.onkey(key="Down", fun=a_snake.down)
-screen.onkey(key="Left", fun=a_snake.left)
-screen.onkey(key="Right", fun=a_snake.right)
+screen.onkey(key="Up", fun=snake.up)
+screen.onkey(key="Down", fun=snake.down)
+screen.onkey(key="Left", fun=snake.left)
+screen.onkey(key="Right", fun=snake.right)
 
 game_over = False
 
 while not game_over:
     screen.update()
     time.sleep(0.1)
-    a_snake.move()
+    snake.move()
+
+    # detect collision with food:
+    if snake.head.distance(food) < 15:
+        food.refresh()
+        scoreboard.update()
+        snake.grown_up()
+
+    # detect collision with wall:
+    if snake.head.xcor() > 285 or snake.head.xcor() < -285 or snake.head.ycor() > 285 or snake.head.ycor() < -285:
+        scoreboard.game_over()
+        game_over = True
+
+    # detect collision with tail:
+    for segment in snake.segments[1:]:
+        if snake.head.distance(segment) < 10:
+            scoreboard.game_over()
+            game_over = True
 
 
 screen.exitonclick()
